adcde410eea5805a957f5931e427496f418c7d14
graholm <graholm@nuevaschool.org>
2016-11-29 08:47:45 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var button = $("deal-with-it-button")
+var button = $(".deal-with-it-button")
 
   button.click (dealWithIt)
 
 function dealWithIt() {
   console.log ("go")
-  $("#glasses").animate({height: "100px"}, 1500)
+  $("#glasses").animate({width: "10000px"}, 1500)
 }
